02b23f7f6991c61d57ef66648e61e9f21d9b91ff
fl0pp5 <fl0pp5@mail.ru>
2021-07-31 20:25:54 +0300

    Add module utils.py   -- make_url(base_url, action) - create route for action   -- make_routes(base_url) - create bunch of routes

---
+++ tew652brp/core/utils.py
@@ -0,0 +1,11 @@
+urls = [
+    'login.ccp',
+]
+
+
+def make_url(base_url, action):
+    return f'{base_url}/{action}'
+
+
+def make_routes(base_url):
+    return {url.split('.')[0]: make_url(base_url, url) for url in urls}
