94e34b2698996dfe3e53b8fd7c9fe19c075ea862
fl0pp5 <fl0pp5@mail.ru>
2021-08-06 20:28:27 +0300

    Update module virtual
    Classes:
        Parser - contains logic for parsing incoming xml data
        Methods:
            find_all_virtual_servers( ... ) - finds all servers
            extract( ... ) - extracts info from xml & returns about server in VServerInfo view
    
    Functions:
        _get_servers( ... ) - returns xml info about virtual servers
        get_virtual_server_list( ... ) - returns info about virtual servers in VServerInfo view

--- tew652brp/core/access/virtual.py
+++ tew652brp/core/access/virtual.py
@@ -1,3 +1,6 @@
+import xml.etree.ElementTree as ET
+
+
 class VServerInfo:
     __slots__ = ('name', 'internal_ip', 'enabled', 'protocol', 'public_port', 'private_port')
 
@@ -16,3 +19,42 @@ class VServerInfo:
                f'protocol: {self.protocol} | ' \
                f'public_port: {self.public_port} | ' \
                f'private_port: {self.private_port}'
+
+
+class Parser:
+    nodes = {
+        'name': 'vsRule_VirtualServerName_',
+        'internal_ip': 'vsRule_InternalIPAddr_',
+        'enabled': 'vsRule_Enable_',
+        'protocol': 'vsRule_Protocol_',
+        'public_port': 'vsRule_PublicPort_',
+        'private_port': 'vsRule_PrivatePort_',
+    }
+
+    keys = nodes.keys()
+    values = nodes.values()
+
+    @staticmethod
+    def find_all_virtual_servers(xml):
+        return ET.fromstring(xml).findall('IGD_WANDevice_i_VirServRule_i_')
+
+    @staticmethod
+    def extract(xml):
+        return VServerInfo(**{key: xml.find(node).text for key, node in zip(Parser.keys, Parser.values)})
+
+
+def _get_servers(session, url):
+    data = {
+        'ccp_act': 'get',
+        'num_inst': '1',
+        'oid_1': 'IGD_WANDevice_i_VirServRule_i_',
+        'inst_1': '11000',
+    }
+    return session.post(url, data=data).text
+
+
+def get_virtual_server_list(session, url):
+    xml = _get_servers(session, url)
+    servers = Parser.find_all_virtual_servers(xml)
+    return [Parser.extract(server) for server in servers]
+
