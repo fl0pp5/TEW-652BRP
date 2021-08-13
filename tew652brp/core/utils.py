43add55dd7dc5cf1efba3188c3705c83ba2f48c2
fl0pp5 <fl0pp5@mail.ru>
2021-08-13 19:05:49 +0300

    Move urls to core.__init__.py

--- tew652brp/core/utils.py
+++ tew652brp/core/utils.py
@@ -1,18 +1,4 @@
-urls = [
-    'login.ccp',
-    'get_set.ccp',
-]
-
-
-def ccp_act(act, **params):
-    def _ccp_act(func):
-        def wrapper(session, url, data):
-            data['ccp_act'] = act
-            for param in params:
-                data[param] = params[param]
-            return func(session, url, data)
-        return wrapper
-    return _ccp_act
+from . import urls
 
 
 def make_url(base_url, action):
