add845451a5e2d3039560da61d75277296dfab8d
fl0pp5 <fl0pp5@mail.ru>
2021-08-13 19:08:37 +0300

    Apply updates to client.py

--- tew652brp/client.py
+++ tew652brp/client.py
@@ -1,13 +1,8 @@
 import requests
 
-from tew652brp.core.login import login
 from tew652brp.core.utils import make_routes
-from tew652brp.core.access.virtual import (
-    set_virtual_server_info,
-    get_servers_xml,
-    xml_to_vserver_info_list,
-    delete_virtual_server,
-)
+from tew652brp.core.login.handler import LoginHandler
+from tew652brp.core.access.virtual.handler import VirtualHandler
 
 
 class Client:
@@ -16,25 +11,5 @@ class Client:
         self._urls = make_routes(base_url)
         self._session = requests.Session()
 
-    def login(self, username, password):
-        return login(self._session, self._urls['login'], username, password)
-
-    def get_virtual_server_list(self):
-        xml = get_servers_xml(self._session, self._urls['get_set'], {
-            'num_inst': '1',
-            'oid_1': 'IGD_WANDevice_i_VirServRule_i_',
-            'inst_1': '11000',
-        })
-        return xml_to_vserver_info_list(xml)
-
-    def set_virtual_server_info(self, server_info):
-        return set_virtual_server_info(self._session, self._urls['get_set'], server_info.to_dict())
-
-    def delete_virtual_server(self, server_info):
-        return delete_virtual_server(self._session, self._urls['get_set'], {
-            'ccpSubEvent': 'CCP_SUB_VIRTUALSERVER',
-            'nextPage': 'virtual_server.htm',
-            'num_inst': '1',
-            'oid_1': 'IGD_WANDevice_i_VirServRule_i_',
-            'inst_1': server_info.instance,
-        })
+        self.virtual = VirtualHandler(self._session, self._base_url)
+        self.login = LoginHandler(self._session, self._base_url)
