124de29f86957b910fc8f06b500661cb30cc90d8
fl0pp5 <fl0pp5@mail.ru>
2021-08-13 19:18:17 +0300

    Update version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
 
 setuptools.setup(
     name='tew652brp',
-    version='0.0.1',
+    version='0.0.2',
     author='fl0pp5',
     description='Interface for working with the TEW-652BRP router API ',
     long_description=readme,
