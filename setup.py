9e3af2dc05a0f55dbfb08eae81c2195965acc525
fl0pp5 <fl0pp5@mail.ru>
2021-08-16 13:59:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,15 +12,23 @@ requirements = [
 
 setuptools.setup(
     name='tew652brp',
-    version='0.0.3',
+    version='0.0.4',
     author='fl0pp5',
     description='Interface for working with the TEW-652BRP router API ',
     long_description=readme,
     long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
-    url='https://github.com/fl0pp5/TEW-652BRP',
+    project_urls={
+        'Documentation': 'https://tew-652brp.readthedocs.io/',
+        'Source': 'https://github.com/fl0pp5/TEW-652BRP',
+    },
     install_requires=requirements,
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
